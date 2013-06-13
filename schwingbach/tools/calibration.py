8724b6b34509838b9475eb39775d38a2bba1b521
philipp <philipp@a735f7ef-1525-764e-8607-acac38503800>
2013-06-13 08:49:25 +0000

    schwingbach: Fixed job:118 by using tzinfo from record.dataset.

--- schwingbach/tools/calibration.py
+++ schwingbach/tools/calibration.py
@@ -37,7 +37,7 @@ class Calibration:
             sourcerecords = self.source.records(self.target.session())
             for sr in sourcerecords:
                 # Change the time of source record from source timezone to the target timezone
-                time = sr.time - self.source.tzinfo.utcoffset(sr.time) + self.target.tzinfo.utcoffset(sr.time) 
+                time = sr.time - sr.dataset.tzinfo.utcoffset(sr.time) + self.target.tzinfo.utcoffset(sr.time) 
                 tv,dt = target.findvalue(time)
                 if dt<=limit: 
                     self.matches.append(Match(sr.time,tv,sr.calibrated,dt))
