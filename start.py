51db8566877782e67525d3e16e76834ef2dc2c1a
Philipp Kraft <16303297+philippkraft@users.noreply.github.com>
2013-10-08 07:57:50 +0000

    schwingbach: start.py kills lockfiles before starting the server

--- start.py
+++ start.py
@@ -10,15 +10,27 @@ Created on 12.02.2012
 from webpage import Root, HeapyPage
 from webpage import lib
 import sys
+import os
+from glob import glob
+
+# Make autoreload
 autoreload= not 'noreload' in sys.argv
 print "autoreload =",autoreload
 
+print "Kill session lock files"
+for fn in glob('webpage/sessions/*.lock'):
+    os.remove(fn)
+
+# Create the URL root object
 root=Root()
 
+# Create a heapy page to view memory usage
 if 'heapy' in sys.argv:
     print "Load heapy"
     from guppy import hpy
     hp=hpy()
     hp.setrelheap()
     root.heapy = HeapyPage(hp)
+    
+# Start the server
 lib.start_server(root, autoreload=autoreload, port=8081)
