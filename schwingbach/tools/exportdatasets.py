c015f21a098f4455d34a4e586881eee5117f64dd
philipp <philipp@a735f7ef-1525-764e-8607-acac38503800>
2013-10-24 08:26:58 +0000

    schwingbach:  - fixed bug that TransformedTimeseries did not transform any more for asarray  - extended iterrecords to return calibrated values fro Timeseries  - Started to extend the exportdatasets.py stuff to include multiple sites (not finished, currently disfunct)

--- schwingbach/tools/exportdatasets.py
+++ schwingbach/tools/exportdatasets.py
@@ -10,19 +10,32 @@ from cStringIO import StringIO
 import codecs
 from traceback import format_exc as traceback
 import sys
+from itertools import product as cartprod
 def exportData(filename,datasetids,start=None,end=None,tolerance=60):
+    """
+    Exports multiple dataset as a csv file with multiple columns
+    Note: Columns are determined by value type and site. Dataset, where site and value type is equal will be appended to each other
+    Note: Only Timeseries are allowed as input, not transformed timeseries (up till now) 
+    """
     session=db.Session()
     try:
+        # Get datasets
         datasets = session.query(db.Timeseries).filter(db.Timeseries.id.in_(datasetids)).order_by(db.Timeseries.start)
         datasetdict = dict((ds.id,ds) for ds in datasets)
+        # Get value types and sites
         vtdict = dict((ds.id,ds._valuetype) for ds in datasets)
+        sitedict = dict((ds.id,ds._site) for ds in datasets)
         valuetypes = sorted(set(ds.valuetype for ds in datasets),key=lambda vt:vt.id)
-        columns = dict((vt.id,i+1) for i,vt in enumerate(valuetypes))
+        sites = sorted(set(ds.site for ds in datasets), key=lambda s:s.id)
+        # Make cartesian product of sites and valuetypes
+        vtsites = list(cartprod(valuetypes,sites))
+        
+        # Derive columns
+        columns = dict((VS[0].id,VS[1].id,i+1) for i,VS in enumerate(vtsites))
+        columnnames=['time'] + ['#%i - %s' % (s.id,vt) for s,vt in vtsites]
+        
+        # Query records
         records = session.query(db.Record).filter(db.Record._dataset.in_(sorted(datasetdict))).order_by(db.Record.time)
-        if start:
-            records = records.filter(db.Record.time>=start)
-        if end:
-            records = records.filter(db.Record.time<=end)    
     except:
         sys.stderr.write(traceback())
         session.close()
@@ -34,15 +47,15 @@ def exportData(filename,datasetids,start=None,end=None,tolerance=60):
         fout = file(filename,'w')
         fout.write(codecs.BOM_UTF8)
         
-        fout.write('time,' + ','.join(unicode(vt).encode('utf-8') for vt in valuetypes) + '\n')
+        fout.write(','.join(unicode(s).encode('utf-8') for s in columnnames) + '\n')
         i=0
-        line = [acttime] + [None] * len(valuetypes)
-        line[0] = acttime
+        line = [acttime] + [None] * len(vtsites)
         old_ds = 0 
         for time,value,dsid in records.values('time','value','dataset'):
             dt = time - acttime
             vt = vtdict[dsid]
-            column = columns[vt]
+            site = sitedict[dsid]
+            column = columns[(vt,site)]
             if dsid!=old_ds:
                 old_ds = dsid
                 ds = datasetdict[dsid]
