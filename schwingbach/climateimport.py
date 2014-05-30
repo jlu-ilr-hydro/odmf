98719e325e40086f75c71c6b1edefdc8094a171e
philipp <philipp@a735f7ef-1525-764e-8607-acac38503800>
2014-05-30 09:56:08 +0000

    schwingbach: Fixed bug in climate.py and some kiosk.html experiments (will be overrridden by next commit)

--- schwingbach/climateimport.py
+++ schwingbach/climateimport.py
@@ -10,47 +10,56 @@ from dataimport.textimport import TextImport, TextImportDescription
 from tools import Path
 import time
 from datetime import datetime
+from traceback import format_exc as traceback
+def log(msg,stream = sys.stdout):
+    stream.write(nowstr() + ' ' + msg + '\n')
+    stream.flush()
+
 if len(sys.argv)!=4:
     print "usage: climateimport.py [newfile.dat] [siteid] [archivefile.dat]"
 
 # Get filename for import from cmdline
 nowstr = lambda : datetime.now().strftime('%Y-%m-%d %H:%M')
 path = Path(sys.argv[1])
-logfile = file(Path(path.up())+'autoimport.log','a')
 i=0
 while not path.exists():
+    log('Wait for ' + path.basename)
     time.sleep(60)
     i+=1
     if i>20:
-        logfile.write(nowstr() + ' After 20 tries does ' + path.basename + ' not exist\n' )
+        log('After 20 tries does ' + path.basename + ' not exist',sys.stderr)
         sys.exit(1)
-# Get siteid from cmdline
-siteid = int(sys.argv[2])
+try:
 
-# Create a TextImport
-ia = TextImport(path.absolute, 'philipp', siteid) 
+    # Get siteid from cmdline
+    siteid = int(sys.argv[2])
+    log('Process settings for ' + path.basename)
 
-# get the configuration of the text import adapter
-config =  ia.descriptor
-# get the datasets o which the imported data is appended
-for col in config.columns:
-    ia.datasets[col.column] = col.append
+    # Create a TextImport
+    ia = TextImport(path.absolute, 'philipp', siteid) 
 
-logfile.write(nowstr() + ' Start to import data from ' + path.basename + '\n')
+    # get the configuration of the text import adapter
+    config =  ia.descriptor
+    # get the datasets o which the imported data is appended
+    for col in config.columns:
+        ia.datasets[col.column] = col.append
 
-# Do the import
-ia.submit()
-# put out data to stdout
-fin = file(path.absolute)
-fout = file(sys.argv[3],'a')
-for i,line in enumerate(fin):
-    if i<config.skiplines:
-        continue
-    else:
-        fout.write(line)
-fin.close()
-fout.close()
-# Kill data file
-os.remove(path.absolute)
-logfile.write(nowstr() + path.basename + ' imported, copied to %s and killed\n' % Path(sys.argv[3]).basename)
-logfile.close()         
+    log('Start to import data from ' + path.basename)
+
+    # Do the import
+    ia.submit()
+    # put out data to stdout
+    fin = file(path.absolute)
+    fout = file(sys.argv[3],'a')
+    for i,line in enumerate(fin):
+        if i<config.skiplines:
+            continue
+        else:
+            fout.write(line)
+    fin.close()
+    fout.close()
+    # Kill data file
+    os.remove(path.absolute)
+    log(path.basename + ' imported, copied to %s and killed' % Path(sys.argv[3]).basename)
+except:
+    log(traceback(),sys.stderr)
