9cb47ede4aea808bd283e6daeac67fb43a137706
chrisw <chrisw@a735f7ef-1525-764e-8607-acac38503800>
2015-11-06 16:11:38 +0000

    Project Object added to sqlalchemy definition

--- schwingbach/db/dbobjects.py
+++ schwingbach/db/dbobjects.py
@@ -405,6 +405,35 @@ class Job(Base):
         else:
             return '#8F8'
 
+class Project(Base):
+    """
+    SqlAlchemy Object for holding project information
+    """
+    __tablename__ = 'project'
+
+    id = sql.Column(sql.Integer, primary_key=True)
+    _person_responsible = sql.Column('person', sql.String, sql.ForeignKey('person.username'))
+    person_responsible = sql.orm.relationship("Person", primaryjoin='Project._person_responsible==Person.username')
+    name = sql.Column(sql.String)
+    comment = sql.Column(sql.String)
+
+    def __str__(self):
+        return u" %s %s: %s %s" % (self.id, self.name, self.person_responsible,
+                                   self.comment)
+
+    def __repr__(self):
+        return u"<Project(id=%s, name=%s, person=%s)>" % \
+               (self.id, self.name, self.person_responsible)
+
+    def __cmp__(self, other):
+        return cmp(self.id, other.id)
+
+    def __jdict__(self):
+        return dict(id=self.id,
+                    name=self.name,
+                    person_responsible=self.person_responsible,
+                    comment=self.comment)
+
 # Creating all tables those inherit Base
 #print "Create Tables"
 #Base.metadata.create_all(engine)
