d30061c01a5bed664407b1857df745548f3560f9
Chris Philip Weber <chris.weber@mni.thm.de>
2018-05-04 18:42:12 +0200

    update copy and mode

--- install.sh
+++ install.sh
@@ -8,4 +8,6 @@ mkdir webpage/sessions
 # schwingbach preferences
 mkdir webpage/preferences
 
-
+# Migrate old preferences and old datafiles to new destination
+# mv $OLD_DIR/webpage/preferences/* ./webpage/preferences
+# mv $OLD_DIR/webpage/datafiles ./webpage
