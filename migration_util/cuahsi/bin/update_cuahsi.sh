c1f741f788ec1fa21464eedfe374b55acf5794d8
Chris Philip Weber <chris.weber@mni.thm.de>
2018-05-16 15:39:49 +0200

    Adds vacuum optimizing after materializing of view rows

--- migration_util/cuahsi/bin/update_cuahsi.sh
+++ migration_util/cuahsi/bin/update_cuahsi.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # update_cuahsi.sh
 
-# Refresh series view
+# Refresh rows of materialized view series
 ./refresh_series.sh
 
 # Force a renewal of the transformed dataset records
 ./../update_transformedvalues.py
+
+# Optimize datastructure of new records
+./vacuum.sh
 
 # Validate the CUAHSI/SBO data
 /check_validity.py
