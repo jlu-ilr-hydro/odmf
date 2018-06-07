d0e2f3046cc14ab34af31a43aa56fcad6cafd555
Chris Philip Weber <chris.weber@mni.thm.de>
2018-06-07 12:48:10 +0200

    Various fixes

---
+++ bin/context.py
@@ -0,0 +1,10 @@
+# As recommended in http://docs.python-guide.org/en/latest/writing/structure/#test-suite
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../odmf')))
+
+
+import tools
+import conf
+
