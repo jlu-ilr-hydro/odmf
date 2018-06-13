cb6e9ae8866311627dbd03c19e8abdae91d0b4df
Chris Philip Weber <chris.weber@mni.thm.de>
2018-06-13 23:49:54 +0200

    Adds offsetyps of datavalues

---
+++ odmf/db/sql/tables/offsettypes.sql
@@ -0,0 +1,26 @@
+-- offsettypes from odm 1.1
+
+DROP TABLE IF EXISTS wof_offsettypes CASCADE;
+
+CREATE TABLE wof_offsettypes (
+  offsettypeid integer,
+  offsetunitsid integer,
+  offsetdescription varchar,
+  CONSTRAINT offsettypeid_pkey PRIMARY KEY (offsettypeid),
+  CONSTRAINT offsetunitsid_fkey FOREIGN KEY (offsetunitsid) REFERENCES units (unitsid)
+);
+
+DROP VIEW IF EXISTS offsettypes;
+
+CREATE VIEW offsettypes AS
+    SELECT 
+     offsettypeid,
+     offsetunitsid,
+     offsetdescription
+    FROM
+     wof_offsettypes;
+
+-- TODO: Not NULL
+
+INSERT INTO wof_offsettypes (offsettypeid, offsetunitsid, offsetdescription) VALUES
+  (0, 52, 'Vertical displacement in meters. Positive means up');
