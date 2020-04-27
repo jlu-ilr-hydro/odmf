16803c491542229bc8cb9d49292d462d1659666e
philippkraft <philipp.kraft@umwelt.uni-giessen.de>
2020-04-27 14:50:10 +0200

    Bootstrapping log.html

--- odmf/webpage/db_editor/log.py
+++ odmf/webpage/db_editor/log.py
@@ -1,8 +1,9 @@
 
 from .. import lib as web
 from ..auth import group, expose_for
-
+from ...config import conf
 from ... import db
+
 from traceback import format_exc as traceback
 from datetime import datetime, timedelta
 
@@ -11,90 +12,86 @@ from datetime import datetime, timedelta
 class LogPage:
 
     @expose_for(group.guest)
-    def default(self, logid=None, siteid=None, lastlogdate=None, days=None):
-        session = db.Session()
-        error = ''
-        queries = dict(
-            persons=session.query(db.Person).order_by(db.Person.can_supervise.desc(), db.Person.surname).all(),
-        )
+    def default(self, logid=None, siteid=None, lastlogdate=None, days=None, error=''):
+        with db.session_scope() as session:
 
-        if logid == 'new':
-            log = db.Log(id=db.newid(db.Log, session),
-                         message='<Log-Beschreibung>', time=datetime.today())
-            user = web.user()
-            if user:
-                log.user = session.query(db.Person).get(user)
-            if siteid:
-                log.site = session.query(db.Site).get(int(siteid))
-            log.time = datetime.today()
-        elif logid is None:
-            log = session.query(db.Log).order_by(
-                db.sql.desc(db.Log.time)).first()
-        else:
-            try:
-                log = session.query(db.Log).get(int(logid))
-            except:
-                error = traceback()
-                log = None
-        if lastlogdate:
-            until = web.parsedate(lastlogdate)
-        else:
-            until = datetime.today()
-        days = web.conv(int, days, 30)
-        loglist = session.query(db.Log).filter(db.Log.time <= until,
-                                               db.Log.time >= until - timedelta(
-                                                   days=days)) \
-            .order_by(db.sql.desc(db.Log.time))
+            if logid == 'new':
+                log = db.Log(id=db.newid(db.Log, session),
+                             message='<Log Message>', time=datetime.today())
+                user = web.user()
+                if user:
+                    log.user = session.query(db.Person).get(user)
+                if siteid:
+                    log.site = session.query(db.Site).get(int(siteid))
+                log.time = datetime.today()
+            elif logid is None:
+                log = session.query(db.Log).order_by(
+                    db.sql.desc(db.Log.time)).first()
+            else:
+                try:
+                    log = session.query(db.Log).get(int(logid))
+                except:
+                    error = traceback()
+                    log = None
+            if lastlogdate:
+                until = web.parsedate(lastlogdate)
+            else:
+                until = datetime.today()
+            days = web.conv(int, days, 30)
+            loglist = session.query(db.Log).filter(db.Log.time <= until,
+                                                   db.Log.time >= until - timedelta(
+                                                       days=days)) \
+                .order_by(db.sql.desc(db.Log.time))
 
-        sitelist = session.query(db.Site).order_by(db.sql.asc(db.Site.id))
+            sitelist = session.query(db.Site).order_by(db.sql.asc(db.Site.id))
+            personlist = session.query(db.Person).order_by(db.Person.can_supervise.desc(), db.Person.surname)
+            return web.render(
+                'log.html', actuallog=log, error=error, db=db,
+                loglist=loglist, sites=sitelist,
+                persons=personlist
+            ).render()
 
-        result = web.render('log.html', actuallog=log, error=error, db=db,
-                            loglist=loglist, sites=sitelist, **queries
-                            ).render()
-
-        session.close()
-        return result
 
     @expose_for(group.logger)
+    @web.method.post_or_put
     def saveitem(self, **kwargs):
         try:
             id = web.conv(int, kwargs.get('id'), '')
         except:
-            return web.render(error=str(kwargs) + '\n' + traceback(), title='Job %s' % kwargs.get('id')
-                              ).render()
+            raise web.redirect('./', error=str(kwargs)).render()
         if 'save' in kwargs:
-            try:
-                session = db.Session()
-                log = session.query(db.Log).get(id)
-                if not log:
-                    log = db.Log(id=id)
-                if kwargs.get('date'):
-                    log.time = datetime.strptime(kwargs['date'], '%d.%m.%Y')
-                log.message = kwargs.get('message')
-                log.user = session.query(db.Person).get(kwargs.get('user'))
-                log.site = session.query(db.Site).get(kwargs.get('site'))
-                session.commit()
-                session.close()
-            except:
-                return web.render('empty.html',
-                                  error=('\n'.join('%s: %s' % it for it in kwargs.items())) + '\n' + traceback(),
-                                  title='Log #%s' % id
-                                  ).render()
+            with db.session_scope() as session:
+                try:
+                    log = session.query(db.Log).get(id)
+                    if not log:
+                        log = db.Log(id=id)
+                    if kwargs.get('date'):
+                        log.time = web.parsedate(kwargs['date'])
+                    log.message = kwargs.get('message')
+                    log.user = session.query(db.Person).get(kwargs.get('user'))
+                    log.site = session.query(db.Site).get(kwargs.get('site'))
+                except:
+                    raise web.redirect(conf.root_url + '/log/' + str(id),
+                                      error=('\n'.join('%s: %s' % it for it in kwargs.items())) + '\n' + traceback(),
+                                      title='Log #%s' % id
+                                      ).render()
         elif 'new' in kwargs:
             id = 'new'
-        raise web.redirect('./%s' % id)
+        raise web.redirect(conf.root_url + '/log/' + str(id))
 
     @expose_for(group.supervisor)
+    @web.method.post_or_put
     def remove(self, id):
-        session = db.Session()
-        log = session.query(db.Log).get(id)
-        if log:
-            session.delete(log)
-            session.commit()
-        raise web.redirect('.')
+        with db.session_scope() as session:
+            log = session.query(db.Log).get(id)
+            if log:
+                session.delete(log)
+                session.commit()
+            raise web.redirect('.')
 
     @expose_for(group.logger)
     @web.mime.json
+    @web.method.get
     def json(self, siteid=None, user=None, old=None, until=None, days=None,
              _=None, keywords=None):
         with db.session_scope() as session:
