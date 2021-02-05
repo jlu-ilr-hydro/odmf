575988458388326ed418f071198bb60f7e9ab684
Philipp Kraft <philipp.kraft@envr.jlug.de>
2021-02-05 18:31:43 +0100

    Fixed whitespace in map.js

--- odmf/static/media/js/map.js
+++ odmf/static/media/js/map.js
@@ -1,258 +1,258 @@
-    // toggle the appeareance of the element with id. Uses jQuery slide
-    function toggle(id) {
-    	$('#'+id).slideToggle('fast');
-    }
-    function setpref(data) {
-			$.ajaxSetup({ scriptCharset:"utf-8", 
-                    contentType:"application/json; charset=utf-8" });
-		  $.post(odmf_ref('/preferences/update'),JSON.stringify(data),null,'json');
-    }
-    // Saves the actual map preferences to the session / file
-		function savemappref() {
-			if (map) {
-				var center = map.getCenter();
-				var data = {item:'map',
-					   			 	lat:center.lat(),
-					   			 	lng:center.lng(),
-					   			 	zoom:map.getZoom(),
-					   			 	type:map.getMapTypeId()
-					   			};
-				setpref(data);
+// toggle the appeareance of the element with id. Uses jQuery slide
+function toggle(id) {
+	$('#'+id).slideToggle('fast');
+}
+function setpref(data) {
+	$.ajaxSetup({ scriptCharset:"utf-8",
+		contentType:"application/json; charset=utf-8" });
+	$.post(odmf_ref('/preferences/update'),JSON.stringify(data),null,'json');
+}
+// Saves the actual map preferences to the session / file
+function savemappref() {
+	if (map) {
+		var center = map.getCenter();
+		var data = {item:'map',
+			lat:center.lat(),
+			lng:center.lng(),
+			zoom:map.getZoom(),
+			type:map.getMapTypeId()
+		};
+		setpref(data);
+	}
+}
+// Shows the actual map coordinates in div #coordinates. Event handler
+function map_mousemove(loc) {
+	$('#coordinates').html(loc.lat().toFixed(6) + '°N, ' + loc.lng().toFixed(6) + '°E');
+}
+// Handles double click on the map
+// If checkbox #createsite is checked, a new site is created, else zoom in
+function map_dblclick(loc) {
+	if ($('#createsite').prop('checked')) {
+		var url = odmf_ref('/site/new');
+		url += '?lat=' + loc.lat();
+		url += '&lon=' + loc.lng();
+		window.location.href = url;
+	} else {
+		map.setCenter(loc);
+		map.setZoom(map.getZoom() + 1);
+	}
+}
+// Clears the markers from the map
+function clearmarker() {
+	$.each(markers,function(index,marker) {
+		marker.setMap(null);
+	});
+	markers.length = 0;
+}
+
+function selectsite(id) {
+	$('#infotext').load(odmf_ref('/map/sitedescription/') + id,
+		function(response, status, xhr) {
+			if (status == "error") {
+				var msg = "Sorry but there was an error: ";
+				$("#infotext").html(msg + xhr.status + " " + xhr.statusText);
 			}
+		});
+	selectedmarker = id;
+	var selectionSymbol = getSelectionSymbol();
+	$.each(markers,function(index,item) {
+		if (item.get('id') == id) {
+			item.setShadow(selectionSymbol);
+		} else {
+			item.setShadow(null);
 		}
-		// Shows the actual map coordinates in div #coordinates. Event handler
-		function map_mousemove(loc) {
-			$('#coordinates').html(loc.lat().toFixed(6) + '°N, ' + loc.lng().toFixed(6) + '°E');
+	});
+	setpref({site:id});
+	$('#title').html('Map <a href="/site/'+ id + '">site #' + id + '</a>');
+
+}
+function zoomToSelected() {
+	var marker=null;
+	alert(markers[0]);
+	$.each(markers,function(index,item){
+		if (item.get('id') == selectedmarker) {
+			marker = item;
+			alert('marker found!' + JSON.stringify(marker));
+			return false;
 		}
-		// Handles double click on the map
-		// If checkbox #createsite is checked, a new site is created, else zoom in
-		function map_dblclick(loc) {
-			if ($('#createsite').prop('checked')) {
-				var url = odmf_ref('/site/new');
-				url += '?lat=' + loc.lat();
-				url += '&lon=' + loc.lng();
-				window.location.href = url; 				
+	});
+	if (marker) {
+		map.setCenter(marker.getPosition());
+		map.setZoom(20);
+	} else {
+		alert('no marker!');
+	}
+}
+function getSelectionSymbol() {
+	return new google.maps.MarkerImage(odmf_ref('/media/mapicons/selection.png'),
+		new google.maps.Size(37,37),
+		new google.maps.Point(0,0),
+		new google.maps.Point(6,30));
+}
+function setmarkers(source,filter) {
+	clearmarker();
+	var selectionsymbol = getSelectionSymbol();
+	$.getJSON(source,filter,function(data) {
+		$.each(data,function(index,item) {
+			if (!item.icon) {
+				icon='unknown.png';
 			} else {
-				map.setCenter(loc);
-				map.setZoom(map.getZoom() + 1);
+				icon = item.icon;
 			}
-		}
-		// Clears the markers from the map
-		function clearmarker() {
-			$.each(markers,function(index,marker) {
-				marker.setMap(null);
-			});
-			markers.length = 0;			
-		}
-		
-    function selectsite(id) {
-        $('#infotext').load(odmf_ref('/map/sitedescription/') + id,
-            function(response, status, xhr) {
-                if (status == "error") {
-                    var msg = "Sorry but there was an error: ";
-                    $("#infotext").html(msg + xhr.status + " " + xhr.statusText);
-                }
-        });
-        selectedmarker = id;
-        var selectionSymbol = getSelectionSymbol();
-				$.each(markers,function(index,item) {
-					if (item.get('id') == id) {
-		  				item.setShadow(selectionSymbol);
-					} else {
-	  					item.setShadow(null);						
-					}
-				});
-				setpref({site:id});
-				$('#title').html('Map <a href="/site/'+ id + '">site #' + id + '</a>');
-
-    }
-    function zoomToSelected() {
-    	var marker=null;
-    	alert(markers[0]);
-    	$.each(markers,function(index,item){
-    		if (item.get('id') == selectedmarker) {
-    			marker = item;
-	    		alert('marker found!' + JSON.stringify(marker));
-    			return false;
-    		}
-    	});
-    	if (marker) {
-    		map.setCenter(marker.getPosition());
-    		map.setZoom(20);
-    	} else {
-    		alert('no marker!');
-    	}
-    }
-		function getSelectionSymbol() {
-			return new google.maps.MarkerImage(odmf_ref('/media/mapicons/selection.png'),
-														new google.maps.Size(37,37),
-														new google.maps.Point(0,0),
-														new google.maps.Point(6,30));
-		}    
-		function setmarkers(source,filter) {
-			clearmarker();
-			var selectionsymbol = getSelectionSymbol(); 
-	    $.getJSON(source,filter,function(data) {
-	    	$.each(data,function(index,item) {
-					if (!item.icon) {
-						icon='unknown.png';
-					} else {
-						icon = item.icon;
-					}
-	    		var image = new google.maps.MarkerImage(odmf_ref('/media/mapicons/') + icon,
-					      new google.maps.Size(24, 24),
-					      new google.maps.Point(0,0),
-					      new google.maps.Point(0, 24));
-		    	var marker = new google.maps.Marker(
-		    		{
-								position: new google.maps.LatLng(item.lat,item.lon),
-	      				title:'#' + item.id + '(' + item.name + ')',
-	      				map:map,
-	      				icon:image,
-	  				}
-	  			);
-	  			marker.set('id',item.id);
-	  			if (item.id == selectedmarker) {
-	  				marker.setShadow(selectionsymbol);
-	  			}
-	  			(function(eventmarker,id){
-		  			google.maps.event.addListener(marker,'click',function() {
-							selectsite(item.id);
-		  			});  				
-	  			}(marker,item.id));
-	  			(function(eventmarker,id){
-	  				google.maps.event.addListener(marker,'dblclick',function() {
-	  					map.setCenter(marker.getPosition());
-	  					map.setZoom(map.getZoom()+2);
-	  				})
-	  			})(marker,item.id);
-	  			markers.push(marker)
-	    	});
-	    });
-		}
-		
-		function popSelect() {
-			let vt = $('#vtselect').val();
-			let user = $('#userselect').val();
-			let date = $('#dateselect').val();
-			let instrument = $('#instrumentselect').val();
-
-			let options = (data, get_value, get_name) => {
-				let html = '<option class="firstoption" value="">Please select...</option>\n';
-				$.each(data, (index, item) => {
-					html += '<option value="' + get_value(item) + '">' + get_name(item) + '</option>\n';
-				});
-				return html
-
-			}
-			let fmt = {
-				id: x=>x ? x.id : '',
-				name: x=>x ? x.name : '',
-				site: x => x ? '#' + x.id + ' (' + x.name + ')' : '#???',
-				user: x=>x.firstname + ' ' + x.surname,
-				self: x => x
-			}
-
-
-			if (vt || user || date) {
-				$('#datasetsonly').prop('checked',true);
-			}
-			$.getJSON(
-				odmf_ref('/dataset/attributes'),
+			var image = new google.maps.MarkerImage(odmf_ref('/media/mapicons/') + icon,
+				new google.maps.Size(24, 24),
+				new google.maps.Point(0,0),
+				new google.maps.Point(0, 24));
+			var marker = new google.maps.Marker(
 				{
-					valuetype:vt,
-					user:user,
-					date:date,
-				},
-				data => {
-					$('#vtselect').html(options(data.valuetype, fmt.id, fmt.name)).val(vt);
-					$('#userselect').html(options(data.measured_by, x=>x.username, fmt.user)).val(user)
+					position: new google.maps.LatLng(item.lat,item.lon),
+					title:'#' + item.id + '(' + item.name + ')',
+					map:map,
+					icon:image,
 				}
 			);
-			$.getJSON(odmf_ref('/site/getinstalledinstruments'),{},function(data){
-				let html=options(data, fmt.id, fmt.name)
-				$('#instrumentselect').html(html).val(instrument);
-			});
-
-			if ($('#datasetsonly').prop('checked')) {
-				setmarkers(odmf_ref('/dataset/attrjson'),
-					{attribute:'site',
-						valuetype:vt,
-						user:user,
-						date:date,
-						instrument:instrument,
-					});
-			} else if (instrument){
-				setmarkers(odmf_ref('/site/with_instrument'),{instrumentid:instrument});
-			} else {
-				setmarkers(odmf_ref('/site/json'),{});
+			marker.set('id',item.id);
+			if (item.id == selectedmarker) {
+				marker.setShadow(selectionsymbol);
 			}
+			(function(eventmarker,id){
+				google.maps.event.addListener(marker,'click',function() {
+					selectsite(item.id);
+				});
+			}(marker,item.id));
+			(function(eventmarker,id){
+				google.maps.event.addListener(marker,'dblclick',function() {
+					map.setCenter(marker.getPosition());
+					map.setZoom(map.getZoom()+2);
+				})
+			})(marker,item.id);
+			markers.push(marker)
+		});
+	});
+}
+
+function popSelect() {
+	let vt = $('#vtselect').val();
+	let user = $('#userselect').val();
+	let date = $('#dateselect').val();
+	let instrument = $('#instrumentselect').val();
+
+	let options = (data, get_value, get_name) => {
+		let html = '<option class="firstoption" value="">Please select...</option>\n';
+		$.each(data, (index, item) => {
+			html += '<option value="' + get_value(item) + '">' + get_name(item) + '</option>\n';
+		});
+		return html
+
+	}
+	let fmt = {
+		id: x=>x ? x.id : '',
+		name: x=>x ? x.name : '',
+		site: x => x ? '#' + x.id + ' (' + x.name + ')' : '#???',
+		user: x=>x.firstname + ' ' + x.surname,
+		self: x => x
+	}
+
+
+	if (vt || user || date) {
+		$('#datasetsonly').prop('checked',true);
+	}
+	$.getJSON(
+		odmf_ref('/dataset/attributes'),
+		{
+			valuetype:vt,
+			user:user,
+			date:date,
+		},
+		data => {
+			$('#vtselect').html(options(data.valuetype, fmt.id, fmt.name)).val(vt);
+			$('#userselect').html(options(data.measured_by, x=>x.username, fmt.user)).val(user)
 		}
-		function clearFilter() {
-			$('.filter').val('');
-			$('#dateselect').val('');
-			$('#datasetsonly').prop('checked',false);
-			popSelect();
-		}
-		function zoomToMarkers() {
-			var latlngbounds = new google.maps.LatLngBounds();
-			$.each(markers,function(index,item){
-   				latlngbounds.extend(item.getPosition());
+	);
+	$.getJSON(odmf_ref('/site/getinstalledinstruments'),{},function(data){
+		let html=options(data, fmt.id, fmt.name)
+		$('#instrumentselect').html(html).val(instrument);
+	});
+
+	if ($('#datasetsonly').prop('checked')) {
+		setmarkers(odmf_ref('/dataset/attrjson'),
+			{attribute:'site',
+				valuetype:vt,
+				user:user,
+				date:date,
+				instrument:instrument,
 			});
-			map.setCenter(latlngbounds.getCenter());
-			map.fitBounds(latlngbounds); 
+	} else if (instrument){
+		setmarkers(odmf_ref('/site/with_instrument'),{instrumentid:instrument});
+	} else {
+		setmarkers(odmf_ref('/site/json'),{});
+	}
+}
+function clearFilter() {
+	$('.filter').val('');
+	$('#dateselect').val('');
+	$('#datasetsonly').prop('checked',false);
+	popSelect();
+}
+function zoomToMarkers() {
+	var latlngbounds = new google.maps.LatLngBounds();
+	$.each(markers,function(index,item){
+		latlngbounds.extend(item.getPosition());
+	});
+	map.setCenter(latlngbounds.getCenter());
+	map.fitBounds(latlngbounds);
+}
+function createmap(lat,lng,zoom,type) {
+	var latlng = new google.maps.LatLng(lat, lng);
+	var mapOptions = {
+		zoom: zoom,
+		center: latlng,
+		mapTypeId: type
+	};
+
+	var map=new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	google.maps.event.addListener(map,'dblclick',function(event) {
+		map_dblclick(event.latLng);
+	});
+	google.maps.event.addListener(map,'mousemove',function(event) {
+		map_mousemove(event.latLng);
+	});
+	return map
+
+}
+
+
+function initMap(site) {
+	map=null;
+
+	$(".datepicker").datepicker({maxDate:"0", dateFormat: 'dd.mm.yy' });
+
+	$.getJSON(odmf_ref('/preferences'), {}, function( data ) {
+		if (site) {
+			data.site = site.id;
+			data.map.zoom = 20;
+			data.map.lat = site.lat;
+			data.map.lng = site.lon;
 		}
-		function createmap(lat,lng,zoom,type) {
- 	    var latlng = new google.maps.LatLng(lat, lng);
-	    var mapOptions = {
-	      zoom: zoom,
-	      center: latlng,
-	      mapTypeId: type
-	    };
-	    
-	    var map=new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-	    google.maps.event.addListener(map,'dblclick',function(event) {
-	    	map_dblclick(event.latLng);
-	    });    
-	    google.maps.event.addListener(map,'mousemove',function(event) {
-	    	map_mousemove(event.latLng);
-	    });    
-	    return map
-			
+
+		map = createmap(data.map.lat,data.map.lng,data.map.zoom,data.map.type);
+
+		markers = [];
+		$('.filter').val('');
+		$('#dateselect').val('');
+		$('#datasetsonly').prop('checked',false);
+
+		if (data.site) {
+			selectsite(data.site);
 		}
-		
 
-    function initMap(site) {
-			map=null;
+		popSelect();
 
-			$(".datepicker").datepicker({maxDate:"0", dateFormat: 'dd.mm.yy' });
+	}).fail(function( jqxhr, textStatus, error){
 
-			$.getJSON(odmf_ref('/preferences'), {}, function( data ) {
-				if (site) {
-					data.site = site.id;
-					data.map.zoom = 20;
-					data.map.lat = site.lat;
-					data.map.lng = site.lon;
-				}
+		$('#map_canvas').html('JSONerror:' + textStatus + ',' + error);
+	});
 
-		    map = createmap(data.map.lat,data.map.lng,data.map.zoom,data.map.type);
-
-		    markers = [];
-				$('.filter').val('');
-				$('#dateselect').val('');
-				$('#datasetsonly').prop('checked',false);
-
- 	    	if (data.site) {
-	          selectsite(data.site);
-    		}
-
-	    	popSelect();
-
-    	}).fail(function( jqxhr, textStatus, error){
-
-    		$('#map_canvas').html('JSONerror:' + textStatus + ',' + error);
-    	});
-
-		$(window).on("beforeunload", savemappref);
-	    // Get map preferences
-    }
+	$(window).on("beforeunload", savemappref);
+	// Get map preferences
+}
