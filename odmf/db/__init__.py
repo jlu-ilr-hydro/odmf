06c232b46205826319def1417f8adee0aedb6a80
Philipp Kraft <philipp.kraft@envr.jlug.de>
2022-06-01 09:21:58 +0200

    Distributed the dbobjects.py file into many files

--- odmf/db/__init__.py
+++ odmf/db/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from .base import Base, Session, engine, session_scope
-from .dbobjects import *
-from .dataset import *
+from .base import Base, Session, engine, session_scope, orm, conf
+from .dbobjects import Person, Image, Project
+from .site import Site, Datasource, Log, Installation
+from .job import Job
+from .dataset import Dataset, DatasetGroup, DatasetItemGetter, Timeseries, Quality, MemRecord, ValueType, Record, TransformedTimeseries
