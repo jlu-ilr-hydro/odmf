4fef049e4dcc3ac85429ac22409d92f237098e87
Philipp Kraft <philipp.kraft@umwelt.uni-giessen.de>
2025-03-14 10:29:58 +0100

    Tests for importing site import #104, missing UI

--- odmf/tools/import_objects.py
+++ odmf/tools/import_objects.py
@@ -17,12 +17,19 @@ class ObjectImportError(Exception):
     ...
 
 
-def import_sites_from_stream(filename: str, stream: BytesIO) -> typing.List[int]:
+def read_df_from_stream(filename: str, stream: typing.BinaryIO) -> pd.DataFrame:
     if filename.endswith('.xlsx'):
-        df = pd.read_excel(stream)
+        return pd.read_excel(stream)
     elif filename.endswith('.csv'):
-        df = pd.read_csv(stream)
-    elif filename.endswith('.geojson'):
+        return pd.read_csv(stream)
+    elif filename.endswith('.parquet'):
+        return pd.read_parquet(stream)
+    else:
+        raise ObjectImportError(f'{filename} is not a supported file type')
+
+
+def import_sites_from_stream(session: db.Session, filename: str, stream: typing.BinaryIO) -> typing.List[int]:
+    if filename.endswith('.geojson'):
         df = gpd.read_file(stream)
         with warnings.catch_warnings():
             df['centroid'] = df.geometry.centroid.to_crs(epsg=4326)
@@ -31,12 +38,12 @@ def import_sites_from_stream(filename: str, stream: BytesIO) -> typing.List[int]
             df['lon'] = df.centroid.x
             df['lat'] = df.centroid.y
     else:
-        raise ObjectImportError(f'{filename} is not a supported file type [.xlsx, .csv, .geojson]')
+        df = read_df_from_stream(filename, stream)
 
-    return import_sites_from_dataframe(df)
+    return import_sites_from_dataframe(session, df)
 
 
-def import_sites_from_dataframe(df: pd.DataFrame) -> typing.List[int]:
+def import_sites_from_dataframe(session, df: pd.DataFrame) -> typing.List[int]:
     """
     Imports sites from a pnadas DataFrame or a GeoDataFrame.
 
@@ -72,23 +79,29 @@ def import_sites_from_dataframe(df: pd.DataFrame) -> typing.List[int]:
     if 'height' not in df.columns:
         df['height'] = None
 
-    with db.session_scope() as session:
-        newid = session.newid(db.Site)
-        df['id'] = newid + df.index
-        df_site = df[['id', 'lat', 'lon', 'height', 'name', 'comment']]
-        df_site.to_sql('site', index=False, con=session.connection(), if_exists='append')
+    newid = db.newid(db.Site, session)
+    df['id'] = newid + df.index
+    df_site = df[['id', 'lat', 'lon', 'height', 'name', 'comment']]
+    df_site.to_sql('site', index=False, con=session.connection(), if_exists='append')
 
-        if type(df) is gpd.GeoDataFrame:
-            df_geo = df['id']
-            df_geo['geojson'] = df.geometry.apply(to_geojson)
-            fields = ['strokewidth', 'strokecolor', 'strokeopacity', 'fillcolor', 'fillopacity']
-            for f in fields:
-                df_geo[f] = df.get(f)
+    if type(df) is gpd.GeoDataFrame:
+        df_geo = pd.DataFrame(index=df.index)
+        df_geo['id'] = df['id']
+        df_geo['geojson'] = df.geometry.apply(to_geojson)
+        for f in ['strokewidth', 'strokecolor', 'strokeopacity', 'fillcolor', 'fillopacity']:
+            df_geo[f] = df.get(f)
 
-            df_geo.to_sql('site_geometry', session.connection(), if_exists='append', index=False)
+        df_geo.to_sql('site_geometry', session.connection(), if_exists='append', index=False)
 
     return list(df['id'])
 
+
+def import_datasets_from_df(session: db.Session, df: pd.DataFrame) -> typing.List[db.Dataset]:
+    """
+    Imports datasets from a table, perform various checks
+    """
+
+    raise ObjectImportError('Importing datasets is not implemented yet')
 
 
 
