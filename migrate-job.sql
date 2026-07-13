24394b39deb55b4d598dc2a9a3c1fb50df5ef7c8
philippkraft <16303297+philippkraft@users.noreply.github.com>
2026-07-13 11:19:24 +0200

    Fixed again mathjax problem, bumped version and organized migration.sql

--- migrate-job.sql
+++ migrate-job.sql
@@ -10,20 +10,22 @@ ALTER TABLE job ADD COLUMN mailer JSON;
 alter table site_geometry drop constraint site_geometry_id_fkey, add constraint site_geometry_id_fkey foreign key (id) references site(id) on delete cascade;
 
 /* Update new Person #200 */
+    ALTER TABLE person    ADD COLUMN orcid VARCHAR NULL;
+    ALTER TABLE person ADD COLUMN last_login TIMESTAMP NULL;
+
+/* Update dataset #257 and #191*/
+ALTER TABLE dataset    ADD COLUMN license VARCHAR NULL;
+ALTER TABLE dataset ADD COLUMN doi VARCHAR NULL;
+    
+ALTER TABLE dataset ALTER COLUMN uses_dst DROP NOT NULL;
+ALTER TABLE datasetalarm    ADD COLUMN name VARCHAR NULL;
+ALTER TABLE datasetalarm    ADD COLUMN message VARCHAR NULL;
+
+
+ALTER TABLE dataset DROP COLUMN uses_dst;
 ALTER TABLE person
     DROP COLUMN supervisor,
     DROP COLUMN telephone,
     DROP COLUMN can_supervise,
     DROP COLUMN mobile,
     DROP COLUMN car_available;
-    ALTER TABLE person    ADD COLUMN orcid VARCHAR NULL;
-    ALTER TABLE person ADD COLUMN last_login TIMESTAMP NULL;
-
-/* Update dataset #257 and #191*/
-ALTER TABLE dataset
-ALTER TABLE dataset    ADD COLUMN license VARCHAR NULL;
-ALTER TABLE dataset ADD COLUMN doi VARCHAR NULL;
-    DROP COLUMN uses_dst;
-ALTER TABLE dataset ALTER COLUMN uses_dst DROP NOT NULL;
-ALTER TABLE datasetalarm    ADD COLUMN name VARCHAR NULL;
-ALTER TABLE datasetalarm    ADD COLUMN message VARCHAR NULL;
